076074ae447d5ff13ac15a73303110584a777f8a
AsierGalindez <asiergalindez@hotmail.com>
2022-01-19 21:18:23 +0100

    finished 17

--- 17_Ejercicio_Repaso03/Program.cs
+++ 17_Ejercicio_Repaso03/Program.cs
@@ -9,7 +9,26 @@ namespace _17_Ejercicio_Repaso03
     {
         static void Main(string[] args)
         {
-            //Introduce aquí tu código
+            Console.WriteLine("escribe un número");
+            string num1text;
+            string num2text;
+            string num3text;
+            
+            int num1;
+            int num2;
+            int num3;
+            num1text = Console.ReadLine();
+            Console.WriteLine("escribe otro número");
+            num2text = Console.ReadLine();
+            Console.WriteLine("escribe otro número");
+            num3text = Console.ReadLine();
+            num1 = Convert.ToInt32(num1text);
+            num2 = Convert.ToInt32(num2text);
+            num3 = Convert.ToInt32(num3text);
+
+            float media = (float) (num1 + num2 + num3) / 3;
+
+            Console.WriteLine("la media es " + media);
         }
     }
 }
